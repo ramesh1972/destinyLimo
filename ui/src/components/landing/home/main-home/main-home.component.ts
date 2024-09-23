1434f3144a253dfc60b99624b711e46202be92a5
ramesh1972 <ramesh_v@trient.com>
2024-09-23 23:19:57 +0530

    ui home

--- ui/src/components/landing/home/main-home/main-home.component.ts
+++ ui/src/components/landing/home/main-home/main-home.component.ts
@@ -1,14 +1,46 @@
 import { Component } from '@angular/core';
-import { RouterLinkActive, RouterLink, RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+import {
+  CarouselCaptionComponent,
+  CarouselComponent,
+  CarouselControlComponent,
+  CarouselIndicatorsComponent,
+  CarouselInnerComponent,
+  CarouselItemComponent,
+  ThemeDirective
+} from '@coreui/angular';
 
 @Component({
   selector: 'app-main-home',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [ThemeDirective, CarouselComponent, CarouselIndicatorsComponent, CarouselInnerComponent, NgFor, CarouselItemComponent, CarouselCaptionComponent, CarouselControlComponent, RouterLink],
   templateUrl: './main-home.component.html',
   styleUrl: './main-home.component.css'
 })
 export class MainHomeComponent {
 
+  slides: any[] = new Array(3).fill({ id: -1, src: '', title: '', subtitle: '' });
+
+  ngOnInit(): void {
+    this.slides[0] = {
+      id: 0,
+      src: '/images/backgrounds/bg3.jpg',
+      title: 'Get your Learning started today!',
+      subtitle: ''
+    };
+    this.slides[1] = {
+      id: 1,
+      src: '/images/backgrounds/bg2.webp',
+      title: 'Help yourself with Online Courses today!',
+      subtitle: ''
+    };
+    this.slides[2] = {
+      id: 2,
+      src: '/images/backgrounds/testbg.jpg',
+      title: 'Test Drive with Exams when you finish!',
+      subtitle: ''
+    };
+  }
 }
--- ui/src/components/landing/home/testimonials/testimonials.component.ts
+++ ui/src/components/landing/home/testimonials/testimonials.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import {
+  CarouselCaptionComponent,
+  CarouselComponent,
+  CarouselControlComponent,
+  CarouselIndicatorsComponent,
+  CarouselInnerComponent,
+  CarouselItemComponent,
+  ThemeDirective
+} from '@coreui/angular';
 
 @Component({
   selector: 'app-testimonials',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, ThemeDirective, CarouselComponent, CarouselIndicatorsComponent, CarouselInnerComponent, CarouselItemComponent, CarouselCaptionComponent, CarouselControlComponent],
   templateUrl: './testimonials.component.html',
   styleUrl: './testimonials.component.css'
 })
